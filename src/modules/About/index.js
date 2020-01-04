cd87db6290c37643979e2f6eaa8b4f5a36621d74
Avinash Singh <avinash.dhillon@gmail.com>
2020-01-04 17:01:46 -0600

    Added picture to about page

--- src/modules/About/index.js
+++ src/modules/About/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Divider from '../../components/Divider';
+import Avinash_Pic from '../../assets/images/Avi.jpg';
+import './About.css';
 
 export const About = () => {
 	return (
 		<div class="componentContainer">
 			<Header text="About" />
 			<Divider />
+			<div className="picContainer">
+				<img src={Avinash_Pic} alt="Avinash Singh" />
+			</div>
 		</div>
 	)
 }
