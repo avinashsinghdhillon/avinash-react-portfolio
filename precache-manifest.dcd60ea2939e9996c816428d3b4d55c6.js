787bbfdf6eaea53f1b3f293c2cd73505560f806a
Avinash Singh <avinash.dhillon@gmail.com>
2020-01-05 18:33:31 -0600

    Updates

---
+++ precache-manifest.dcd60ea2939e9996c816428d3b4d55c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70fbb064c6c3590aa3614ae82a35b95",
+    "url": "/avinash-react-portfolio/index.html"
+  },
+  {
+    "revision": "055eef8513e33c9e1b23",
+    "url": "/avinash-react-portfolio/static/css/main.df9cbc8f.chunk.css"
+  },
+  {
+    "revision": "f4ea1f065c9cacabb7a9",
+    "url": "/avinash-react-portfolio/static/js/2.6990802f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/avinash-react-portfolio/static/js/2.6990802f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "055eef8513e33c9e1b23",
+    "url": "/avinash-react-portfolio/static/js/main.fc18c9d0.chunk.js"
+  },
+  {
+    "revision": "5bd47d0aae5f83636630",
+    "url": "/avinash-react-portfolio/static/js/runtime-main.f8f9db13.js"
+  },
+  {
+    "revision": "199968d76257956193f138fd8bf260e6",
+    "url": "/avinash-react-portfolio/static/media/AviPic.199968d7.png"
+  },
+  {
+    "revision": "1e9f07311aab5d40ce641f58ee259649",
+    "url": "/avinash-react-portfolio/static/media/ZXSpectrum16x9.1e9f0731.jpg"
+  },
+  {
+    "revision": "75282357ca0c32652f273bf8d21c4c95",
+    "url": "/avinash-react-portfolio/static/media/bamazon.75282357.png"
+  },
+  {
+    "revision": "bb35835515910af295378d59ac97548f",
+    "url": "/avinash-react-portfolio/static/media/endemicDoctor.bb358355.gif"
+  },
+  {
+    "revision": "f2632d4afae81840f1edd1fcb665ac02",
+    "url": "/avinash-react-portfolio/static/media/liri.f2632d4a.png"
+  },
+  {
+    "revision": "27963220326d58157d1c78958d6e16df",
+    "url": "/avinash-react-portfolio/static/media/rpsMultiplayer.27963220.jpg"
+  },
+  {
+    "revision": "eb8b0eb13f7de5313232190a08d84b0d",
+    "url": "/avinash-react-portfolio/static/media/starWarsRPG.eb8b0eb1.jpeg"
+  },
+  {
+    "revision": "77064555f469c86255a3f638541b7891",
+    "url": "/avinash-react-portfolio/static/media/triviaGame.77064555.jpg"
+  }
+]);
