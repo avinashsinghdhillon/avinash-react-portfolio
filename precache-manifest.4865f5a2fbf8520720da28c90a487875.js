8a215fedb5a2218be5236d086cc346dad75d08cf
Avinash Singh <avinash.dhillon@gmail.com>
2020-01-18 11:57:56 -0600

    Updates

---
+++ precache-manifest.4865f5a2fbf8520720da28c90a487875.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2365ccaf4f0bb1547d309973f458e18",
+    "url": "/avinash-react-portfolio/index.html"
+  },
+  {
+    "revision": "66dabf5f0e3f84b7c3f8",
+    "url": "/avinash-react-portfolio/static/css/main.501f60de.chunk.css"
+  },
+  {
+    "revision": "f4ea1f065c9cacabb7a9",
+    "url": "/avinash-react-portfolio/static/js/2.6990802f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/avinash-react-portfolio/static/js/2.6990802f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66dabf5f0e3f84b7c3f8",
+    "url": "/avinash-react-portfolio/static/js/main.a9011715.chunk.js"
+  },
+  {
+    "revision": "5bd47d0aae5f83636630",
+    "url": "/avinash-react-portfolio/static/js/runtime-main.f8f9db13.js"
+  },
+  {
+    "revision": "199968d76257956193f138fd8bf260e6",
+    "url": "/avinash-react-portfolio/static/media/AviPic.199968d7.png"
+  },
+  {
+    "revision": "f8faa4bf5c5837219351d00617ae8e85",
+    "url": "/avinash-react-portfolio/static/media/MemoryGame.f8faa4bf.JPG"
+  },
+  {
+    "revision": "1e9f07311aab5d40ce641f58ee259649",
+    "url": "/avinash-react-portfolio/static/media/ZXSpectrum16x9.1e9f0731.jpg"
+  },
+  {
+    "revision": "75282357ca0c32652f273bf8d21c4c95",
+    "url": "/avinash-react-portfolio/static/media/bamazon.75282357.png"
+  },
+  {
+    "revision": "bb35835515910af295378d59ac97548f",
+    "url": "/avinash-react-portfolio/static/media/endemicDoctor.bb358355.gif"
+  },
+  {
+    "revision": "f2632d4afae81840f1edd1fcb665ac02",
+    "url": "/avinash-react-portfolio/static/media/liri.f2632d4a.png"
+  },
+  {
+    "revision": "27963220326d58157d1c78958d6e16df",
+    "url": "/avinash-react-portfolio/static/media/rpsMultiplayer.27963220.jpg"
+  },
+  {
+    "revision": "eb8b0eb13f7de5313232190a08d84b0d",
+    "url": "/avinash-react-portfolio/static/media/starWarsRPG.eb8b0eb1.jpeg"
+  },
+  {
+    "revision": "77064555f469c86255a3f638541b7891",
+    "url": "/avinash-react-portfolio/static/media/triviaGame.77064555.jpg"
+  }
+]);
--- src/components/Divider/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Divider.css';
-
-export const Divider = () => (
-    <div className="divider" />
-)
-
-export default Divider
